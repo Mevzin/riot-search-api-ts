426183b77fe0c51f62af4e2e0f1d954966faf2f6
Thiago Torres de Souza <thiagomev@gmail.com>
2024-04-10 11:03:37 -0300

    Remove key in service

--- src/service/apiRiot.ts
+++ src/service/apiRiot.ts
@@ -5,5 +5,5 @@ import { routesApi } from './routesRiotApi';
 // /riot/account/v1/accounts/by-riot-id/{gameName}/{tagLine}
 export const searchByTagline = axios.create({
     baseURL: routesApi.searchByNick,
-    headers: { "X-Riot-Token": "RGAPI-715fe5e3-3553-4fdd-98bf-06ebf3df7edf" }
+    headers: { "X-Riot-Token": process.env.API_KEY }
 })
